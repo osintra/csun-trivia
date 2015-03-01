79ace73ad43a7118ba53a903dcf97119b6b8e2b4
VP <v@osintra.com>
2015-02-28 23:44:32 -0800

    testing git

--- CsunMapTrivia/app/src/main/java/com/comp680team2/csunmaptrivia/MapsActivity.java
+++ CsunMapTrivia/app/src/main/java/com/comp680team2/csunmaptrivia/MapsActivity.java
@@ -5,9 +5,13 @@
 
 package com.comp680team2.csunmaptrivia;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,15 +23,69 @@ import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    double latitude = 0;
+    double longitude = 0;
+    double vertx[] = {34.239095, 34.239104, 34.238452, 34.238452};
+    double verty[] = {-118.530277, -118.530143, -118.530143, -118.530277};
+    int seconds = 10;
+    TextView timerTextView;
+    Polygon polygon = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps_activity);
-        setUpMapIfNeeded();
+        timerTextView = (TextView) findViewById(R.id.timer);
+
+        Thread backgroundThread = new Thread(new Runnable() {
+            public void run() {
+
+                while (true) {
+
+
+                    synchronized (this) {
+                        try {
+                            wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                timerTextView.setText(Integer.toString(seconds));
+                                seconds--;
+
+                            }
+                        });
+
+                    }
+
+                }
+
+
+            }
+        });
+        backgroundThread.start();
     }
 
     @Override
@@ -71,12 +129,33 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        LatLng ll = new LatLng(34.243324, -118.529543);
-        float zoom = 13;
-        mMap.addMarker(new MarkerOptions().position(ll).title("Marker"));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        double x = 34.240089;
+        double y = -118.529435;
+        LatLng ll = new LatLng(x, y);
+        float zoom = 17;
+        mMap.addMarker(new MarkerOptions().position(ll).title("CSUN"));
 
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, zoom));
+
+        int sides = 4;
+
+        boolean result = pnpoly(sides, vertx, verty, x, y);
+
+
+        if (result) {
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(34.239095, -118.530277), new LatLng(34.239104, -118.530143), new LatLng(34.238452, -118.530143), new LatLng(34.238452, -118.530277))
+                    .strokeColor(Color.GREEN)
+                    .fillColor(Color.BLUE));
+
+        } else {
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(34.239095, -118.530277), new LatLng(34.239104, -118.530143), new LatLng(34.238452, -118.530143), new LatLng(34.238452, -118.530277))
+                    .strokeColor(Color.RED)
+                    .fillColor(Color.BLUE));
+        }
 /*
         PolylineOptions rectOptions = new PolylineOptions()
                 .add(new LatLng(34.24123938043965, -118.53193402290344))
@@ -86,9 +165,149 @@ public class MapsActivity extends FragmentActivity {
                 .add(new LatLng(34.241257118975916, -118.53331804275513)); // Closes the polyline.
         Polyline polyline = mMap.addPolyline(rectOptions);
 */
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(final LatLng point) {
+                MapsActivity.this.latitude = point.latitude;
+                MapsActivity.this.longitude = point.longitude;
+                if(!(MapsActivity.this.latitude == 0.0 && MapsActivity.this.longitude == 0.0))
+                {
+                    polygon.remove();
+                    boolean myresult = pnpoly(4,vertx, verty,MapsActivity.this.latitude,MapsActivity.this.longitude);
+                    polygonDraw(myresult);
+                }
+
+            }
+
+
+        });
+
+
+        /*
         Polygon polygon = mMap.addPolygon(new PolygonOptions()
-                .add(new LatLng(34.241239380333965, -118.53193402290344), new LatLng(34.2419134421894, -118.53192329406738), new LatLng(34.241931180583606, -118.53333950042725), new LatLng(34.241257118975916, -118.53331804275513))
+                .add(new LatLng(34.24123938043965, -118.53193402290344), new LatLng(34.2419134421894, -118.53192329406738), new LatLng(34.241931180583606, -118.53333950042725), new LatLng(34.241257118975916, -118.53331804275513))
                 .strokeColor(Color.RED)
                 .fillColor(Color.BLUE));
+        DoPOST mDoPOST = new DoPOST(this, "yo");
+        mDoPOST.execute("");*/
+    }
+    public void polygonDraw (boolean result) {
+        if(result) {
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(34.239095, -118.530277), new LatLng(34.239104, -118.530143), new LatLng(34.238452, -118.530143), new LatLng(34.238452, -118.530277))
+                    .strokeColor(Color.GREEN)
+                    .fillColor(Color.BLUE));
+        } else {
+            polygon = mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(34.239095, -118.530277), new LatLng(34.239104, -118.530143), new LatLng(34.238452, -118.530143), new LatLng(34.238452, -118.530277))
+                    .strokeColor(Color.RED)
+                    .fillColor(Color.BLUE));
+        }
+    }
+
+    boolean pnpoly(int nvert, double vertx[], double verty[], double testx, double testy)
+    {
+        int i, j;
+        int flag =0;
+        char check;
+        boolean c = false;
+
+        for (i=0; i< nvert; i++) {
+            if(testy == verty[i]) {
+                if(testx < vertx[0] && testx > vertx[2]) {
+                    c = true;
+                    return c;
+                } else {
+                    for (j=0; j< nvert; j++) {
+                        if(testx == vertx[j]){
+                            c = true;
+                            return c;
+                        }
+                    }
+                }
+            } else if(testx == vertx[i]){
+                if(testy > verty[0] && testy < verty[1]){
+                    c = true;
+                    return c;
+                } else {
+                    for (j=0; j< nvert; j++) {
+                        if(testy == verty[j]){
+                            c = true;
+                            return c;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (i = 0, j = nvert-1; i < nvert; j = i++) {
+            if ( ((verty[i]>testy) != (verty[j]>testy)) &&
+                    (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) ) {
+                c = !c;
+            }
+        }
+        return c;
+    }
+
+// -- Rest Example. Tested to work.
+    public class DoPOST extends AsyncTask<String, Void, Boolean> {
+
+        Context mContext = null;
+        String strNameToSearch = "";
+
+        //Result data
+        String strFirstName;
+        String strLastName;
+        int intAge;
+        int intPoints;
+
+        Exception exception = null;
+
+        DoPOST(Context context, String nameToSearch) {
+            mContext = context;
+            strNameToSearch = nameToSearch;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... arg0) {
+
+            try {
+
+                //Setup the parameters
+                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("FirstNameToSearch", strNameToSearch));
+                //Add more parameters as necessary
+
+                //Create the HTTP request
+                HttpParams httpParameters = new BasicHttpParams();
+
+                //Setup timeouts
+                HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
+                HttpConnectionParams.setSoTimeout(httpParameters, 15000);
+
+                HttpClient httpclient = new DefaultHttpClient(httpParameters);
+                HttpPost httppost = new HttpPost("http://nullroute.cc/rest_test/login.php");
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+
+                String result = EntityUtils.toString(entity);
+
+                // Create a JSON object from the request response
+                JSONObject jsonObject = new JSONObject(result);
+
+                //Retrieve the data from the JSON object
+                strFirstName = jsonObject.getString("FirstName");
+                strLastName = jsonObject.getString("LastName");
+                intAge = jsonObject.getInt("Age");
+                intPoints = jsonObject.getInt("Points");
+
+            } catch (Exception e) {
+                Log.e("ClientServerDemo", "Error:", e);
+                exception = e;
+            }
+
+            return true;
+        }
     }
 }
