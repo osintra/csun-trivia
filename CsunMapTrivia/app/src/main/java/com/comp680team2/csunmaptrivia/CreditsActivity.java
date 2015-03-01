413cd1654a982c0f5ee4dfb9d0bc5e14af705613
RenegadeX135 <chris.g.bowles@gmail.com>
2015-02-28 22:23:34 -0800

    Added content to the Credits Activity # CreditsActivity.java, credits_activity.xml and strings.xml
    Added an "ActionBar" to the app by importing new styles into the project
    + /values/styles.xml and /values-v21/styles.xml
    # Minor edits in main_activity.xml to accommodate spacing differences

--- CsunMapTrivia/app/src/main/java/com/comp680team2/csunmaptrivia/CreditsActivity.java
+++ CsunMapTrivia/app/src/main/java/com/comp680team2/csunmaptrivia/CreditsActivity.java
@@ -6,7 +6,11 @@
 package com.comp680team2.csunmaptrivia;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreditsActivity extends Activity
 {
@@ -14,5 +18,23 @@ public class CreditsActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credits_activity);
+
+		Button button1 = (Button)findViewById(R.id.creditsButton1);
+		button1.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View view)
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nullroute.cc/")));
+			}
+		});
+
+		Button button2 = (Button)findViewById(R.id.creditsButton2);
+		button2.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View view)
+			{
+				finish();
+			}
+		});
 	}
 }
