de24f4ffb99509c31cff251cefff300bde4014f6
russellmt <russell.templet.109@my.csun.edu>
2015-04-19 11:40:26 -0700

    Difficulty moved / Added reference activity husk

---
+++ CsunMapTrivia/app/src/main/java/com/comp680team2/csunmaptrivia/ReferencesActivity.java
@@ -0,0 +1,16 @@
+package com.comp680team2.csunmaptrivia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ReferencesActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.references_activity);
+    }
+}
